87eb042dcfc1adcde11bb5e24897442937f6f2a4
Robin <rob.blanquart02100@gmail.com>
2022-04-28 15:27:39 +0200

    navigation bottom bar added

--- src/Components/BottomBar/BottomBar.tsx
+++ src/Components/BottomBar/BottomBar.tsx
@@ -3,6 +3,16 @@ import { FC } from "react";
 import styles from "./BottomBar.styles";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import Feather from "react-native-vector-icons/Feather";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RouteParams } from "../../Navigation/Navigation";
+
+type Props = {
+  title?: string;
+  icon: string;
+  iconType?: string;
+  to: string;
+};
 
 const Items = [
   {
@@ -10,37 +20,36 @@ const Items = [
     title: "Films",
     icon: "film",
     iconType: "FontAwesome5",
+    to: "Home",
   },
   {
     search: false,
     title: "Favoris",
     icon: "heart",
     iconType: "FontAwesome5",
+    to: "Favorites",
   },
   {
     search: true,
     icon: "search",
     iconType: "Feather",
+    to: "Home",
   },
   {
     search: false,
     title: "Profil",
     icon: "user",
     iconType: "Feather",
+    to: "",
   },
   {
     search: false,
     title: "Options",
     icon: "settings",
     iconType: "Feather",
+    to: "",
   },
 ];
-
-type Props = {
-  title?: string;
-  icon: string;
-  iconType?: string;
-};
 
 const BottomBar: FC = () => {
   return (
@@ -52,12 +61,14 @@ const BottomBar: FC = () => {
               title={item.title}
               icon={item.icon}
               iconType={item.iconType}
+              to={item.to}
             />
           ) : (
             <MenuItems
               title={item.title}
               icon={item.icon}
               iconType={item.iconType}
+              to={item.to}
             />
           )}
         </View>
@@ -66,23 +77,37 @@ const BottomBar: FC = () => {
   );
 };
 
-const MenuItems: FC<Props> = ({ title, icon, iconType }) => (
-  <TouchableOpacity style={styles.menuItems__container}>
-    {iconType === "Feather" ? (
-      <Feather name={icon} size={20} style={styles.icon} />
-    ) : (
-      <FontAwesome5 name={icon} size={20} style={styles.icon} />
-    )}
-    <Text style={styles.text}>{title}</Text>
-  </TouchableOpacity>
-);
+const MenuItems: FC<Props> = ({ title, icon, iconType, to }) => {
+  const navigation = useNavigation<NativeStackNavigationProp<RouteParams>>();
 
-const SearchItem: FC<Props> = ({ icon }) => (
-  <View style={styles.menuItems__container}>
-    <TouchableOpacity style={styles.icon__container}>
-      <Feather name={icon} size={25} style={styles.icon} />
+  return (
+    <TouchableOpacity
+      onPress={() => navigation.navigate(`${to}` as keyof RouteParams)}
+      style={styles.menuItems__container}
+    >
+      {iconType === "Feather" ? (
+        <Feather name={icon} size={20} style={styles.icon} />
+      ) : (
+        <FontAwesome5 name={icon} size={20} style={styles.icon} />
+      )}
+      <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
-  </View>
-);
+  );
+};
+
+const SearchItem: FC<Props> = ({ icon, to }) => {
+  const navigation = useNavigation<NativeStackNavigationProp<RouteParams>>();
+
+  return (
+    <View style={styles.menuItems__container}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate(`${to}` as keyof RouteParams)}
+        style={styles.icon__container}
+      >
+        <Feather name={icon} size={25} style={styles.icon} />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default BottomBar;
